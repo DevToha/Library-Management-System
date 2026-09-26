270b984c791281b32241f29429ab0918928af418
sudiptohalderrahuloffical-alt <sudiptohalderrahuloffical@gmail.com>
2026-09-26 21:07:22 +0600

    Add files via upload

---
+++ Periodical.java
@@ -0,0 +1,23 @@
+package model;
+
+public class Periodical extends Item {
+    private String issueNumber;
+    private String category;
+
+    public Periodical(String id, String title, String issueNumber, String category) {
+        super(id, title);
+        this.issueNumber = issueNumber;
+        this.category = category;
+    }
+
+    public String getIssueNumber() { return issueNumber; }
+    public void setIssueNumber(String issueNumber) { this.issueNumber = issueNumber; }
+
+    public String getCategory() { return category; }
+    public void setCategory(String category) { this.category = category; }
+
+    @Override
+    public String getItemType() {
+        return "Periodical";
+    }
+}

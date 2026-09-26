768df24e4f74ca629cdb5b2896dae75581fbdd0a
Toha Khan <tohakhan@Tohas-MacBook-Pro-M5.local>
2026-09-26 17:40:16 +0600

    Creating Member class extending Person and Adding Abstract Item class for library resources

--- src/model/Item.java
+++ src/model/Item.java
@@ -1,4 +1,28 @@
 package model;
 
-public class Item {
+import java.io.Serializable;
+
+public abstract class Item implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private String id;
+    private String title;
+    private boolean isAvailable;
+
+    public Item(String id, String title) {
+        this.id = id;
+        this.title = title;
+        this.isAvailable = true;
+    }
+
+    public String getId() { return id; }
+    public void setId(String id) { this.id = id; }
+
+    public String getTitle() { return title; }
+    public void setTitle(String title) { this.title = title; }
+
+    public boolean isAvailable() { return isAvailable; }
+    public void setAvailable(boolean available) { isAvailable = available; }
+
+    public abstract String getItemType();
 }
--- src/model/Member.java
+++ src/model/Member.java
@@ -1,4 +1,18 @@
 package model;
 
-public class Member {
+public class Member extends Person {
+    private String memberType;
+    private int maxAllowedItems;
+
+    public Member(String id, String name, String email, String memberType) {
+        super(id, name, email);
+        this.memberType = memberType;
+        this.maxAllowedItems = memberType.equalsIgnoreCase("Faculty") ? 10 : 5;
+    }
+
+    public String getMemberType() { return memberType; }
+    public void setMemberType(String memberType) { this.memberType = memberType; }
+
+    public int getMaxAllowedItems() { return maxAllowedItems; }
+    public void setMaxAllowedItems(int maxAllowedItems) { this.maxAllowedItems = maxAllowedItems; }
 }

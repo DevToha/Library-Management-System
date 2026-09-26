1d6adf1a0d63761612afe7b6a0c2f108a3334652
Arafat Rahman <araf254094@gmail.com>
2026-09-26 21:06:35 +0600

    Checking

--- src/model/DigitalMedia.java
+++ src/model/DigitalMedia.java
@@ -1,4 +1,23 @@
 package model;
 
-public class DigitalMedia {
+public class DigitalMedia extends Item {
+    private String format;
+    private double fileSizeMB;
+
+    public DigitalMedia(String id, String title, String format, double fileSizeMB) {
+        super(id, title);
+        this.format = format;
+        this.fileSizeMB = fileSizeMB;
+    }
+
+    public String getFormat() { return format; }
+    public void setFormat(String format) { this.format = format; }
+
+    public double getFileSizeMB() { return fileSizeMB; }
+    public void setFileSizeMB(double fileSizeMB) { this.fileSizeMB = fileSizeMB; }
+
+    @Override
+    public String getItemType() {
+        return "DigitalMedia";
+    }
 }

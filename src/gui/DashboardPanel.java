cba04daebe824087fdc6d87b1ca4c881295ef399
C Sharp Technology <sudiptohalderrahuloffical@gmail.com>
2026-09-26 22:56:17 +0600

    Added Periodical model to system

--- src/gui/DashboardPanel.java
+++ src/gui/DashboardPanel.java
@@ -1,4 +1,30 @@
 package gui;
 
-public class DashboardPanel {
+import service.LibraryService;
+
+public class DashboardPanel extends JPanel {
+
+
+    public DashboardPanel(LibraryService service) {
+        this.service = service;
+        setLayout();
+        setBorder(BorderFactory.createEmptyBorder());
+
+        totalItemsLabel = createCard("Total Items", "0");
+        totalMembersLabel = createCard("Registered Members", "0");
+        activeTxLabel = createCard("Active Loans", "0");
+
+        add(totalItemsLabel);
+        add(totalMembersLabel);
+        add(activeTxLabel);
+
+        refresh();
+    }
+
+    private JLabel createCard(String title, String initialValue) {
+        label.setOpaque(true);
+        label.setBackground();
+        label.setBorder(BorderFactory.createLineBorder());
+        return label;
+    }
 }

4161b6dfa650b5b30018ee3654d4b3e4891ad3a1
Toha Khan <tohakhan@Tohas-MacBook-Pro-M5.local>
2026-09-26 17:34:52 +0600

    Adding base Person class with common attributes

--- src/model/Person.java
+++ src/model/Person.java
@@ -1,4 +1,31 @@
 package model;
 
-public class Person {
+import java.io.Serializable;
+
+public abstract class Person implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private String id;
+    private String name;
+    private String email;
+
+    public Person(String id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    public String getId() { return id; }
+    public void setId(String id) { this.id = id; }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+
+    @Override
+    public String toString() {
+        return name + " (ID: " + id + ")";
+    }
 }
